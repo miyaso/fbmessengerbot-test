775f9f330337866c1b19b4304f792f1571ff8c8b
So Miyahara <miyaso0628@yahoo.co.jp>
2016-05-26 00:45:35 +0900

    bug fix

--- web/index.php
+++ web/index.php
@@ -30,54 +30,55 @@ $app->post('/callback', function (Request $request) use ($app) {
     $body = json_decode($request->getContent(), true);
     $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.6/']);
 
-    $obj = $body['entry'][0];
-    $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
+    foreach($body['entry'] as $obj){
+        $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
 
-    $m = $obj['messaging'][0];
-    $app['monolog']->addInfo(sprintf('messaging: %s', json_encode($m)));
-    $from = $m['sender']['id'];
-    $text = $m['message']['text'];
+        foreach($obj['messaging'] as $m){
+            $app['monolog']->addInfo(sprintf('messaging: %s', json_encode($m)));
+            $from = $m['sender']['id'];
+            $text = $m['message']['text'];
 
-    if ($text) {
-        $json = [
-            'recipient' => [
-                'id' => $from, 
-            ],
-            'message' => [
-                'text' => 'ちょっとまってね！', 
-            ],
-        ];
-        $client->request('POST', $path, ['json' => $json]);
-        // 
-        $pom_key = getenv('POM_KEY');
-        $url = 'http://ws.ponpare.jp/ws/wsp0100/Wst0201Action.do?key='.$pom_key.'&large_area=1&format=json';
-        $json = file_get_contents($url);
-        $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-        $eq_data = json_decode($json,true);
-        // 
-        $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
-        for ($i = 0; $i < 3; $i++) {
-            $c_json = [
-                'recipient' => [
-                    'id' => $from, 
-                ],
-                'message' => [
-                    'text' => sprintf('商品名:%s', $eq_data["ticket"][$i]["name"]), 
-                ],
-            ];
-            $client->request('POST', $path, ['json' => $c_json]);
+            if ($text) {
+                $json = [
+                    'recipient' => [
+                        'id' => $from, 
+                    ],
+                    'message' => [
+                        'text' => 'ちょっとまってね！', 
+                    ],
+                ];
+                $client->request('POST', $path, ['json' => $json]);
+                // 
+                $pom_key = getenv('POM_KEY');
+                $url = 'http://ws.ponpare.jp/ws/wsp0100/Wst0201Action.do?key='.$pom_key.'&large_area=1&format=json';
+                $json = file_get_contents($url);
+                $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+                $eq_data = json_decode($json,true);
+                // 
+                $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                for ($i = 0; $i < 3; $i++) {
+                    $c_json = [
+                        'recipient' => [
+                            'id' => $from, 
+                        ],
+                        'message' => [
+                            'text' => sprintf('商品名:%s', $eq_data["ticket"][$i]["name"]), 
+                        ],
+                    ];
+                    $client->request('POST', $path, ['json' => $c_json]);
+                }
+                $json = [
+                    'recipient' => [
+                        'id' => $from, 
+                    ],
+                    'message' => [
+                        'text' => sprintf('%sじゃない', $text), 
+                    ],
+                ];
+                $client->request('POST', $path, ['json' => $json]);
+            }
         }
-        $json = [
-            'recipient' => [
-                'id' => $from, 
-            ],
-            'message' => [
-                'text' => sprintf('%sじゃない', $text), 
-            ],
-        ];
-        $client->request('POST', $path, ['json' => $json]);
     }
-
     return 0;
 });
 
