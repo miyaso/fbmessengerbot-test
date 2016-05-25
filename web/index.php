77d458a2f1f47dc7cf92b4dffde68c42f740b990
So Miyahara <miyaso0628@yahoo.co.jp>
2016-05-25 23:55:27 +0900

    bug fix

--- web/index.php
+++ web/index.php
@@ -30,34 +30,45 @@ $app->post('/callback', function (Request $request) use ($app) {
     $body = json_decode($request->getContent(), true);
     $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.6/']);
 
-    foreach ($body['entry'] as $obj) {
-        $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
+    $obj = $body['entry'] 
+    $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
 
-        foreach ($obj['messaging'] as $m) {
-            $app['monolog']->addInfo(sprintf('messaging: %s', json_encode($m)));
-            $from = $m['sender']['id'];
-            $text = $m['message']['text'];
+    $m = $obj['messaging']
+    $app['monolog']->addInfo(sprintf('messaging: %s', json_encode($m)));
+    $from = $m['sender']['id'];
+    $text = $m['message']['text'];
 
-            if ($text) {
-                $pom_key = 'wye154e206fea5';
-                $url = 'http://ws.ponpare.jp/ws/wsp0100/Wst0201Action.do?key='.$pom_key.'&large_area=1&format=json';
-                $json = file_get_contents($url);
-                $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-                $eq_data = json_decode($json,true);
-                $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
-                $json = [
-                    'recipient' => [
-                        'id' => $from, 
-                    ],
-                    'message' => [
-                        'text' => sprintf('%sじゃない', $text), 
-                    ],
-                ];
-                $client->request('POST', $path, ['json' => $json]);
-            }
+    if ($text) {
+        // 
+        $pom_key = getenv('POM_KEY');
+        $url = 'http://ws.ponpare.jp/ws/wsp0100/Wst0201Action.do?key='.$pom_key.'&large_area=1&format=json';
+        $json = file_get_contents($url);
+        $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+        $eq_data = json_decode($json,true);
+        // 
+        $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+        for ($i = 0; $i < 3; $i++) {
+            $c_json = [
+                'recipient' => [
+                    'id' => $from, 
+                ],
+                'message' => [
+                    'text' => sprintf('商品名:%s', $eq_data["ticket"][$i]["name"]), 
+                ],
+            ];
+            $client->request('POST', $path, ['json' => $c_json]);
         }
-
+        $json = [
+            'recipient' => [
+                'id' => $from, 
+            ],
+            'message' => [
+                'text' => sprintf('%sじゃない', $text), 
+            ],
+        ];
+        $client->request('POST', $path, ['json' => $json]);
     }
+
 
     return 0;
 });
